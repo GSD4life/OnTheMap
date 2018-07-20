e5c0d1ca8c4c94cc309f50f28037499fb37800f4
Shane Sealy <35928028+GSD4life@users.noreply.github.com>
2018-07-20 11:02:59 -0400

    Added image set,  blackbox swift file, refined appDelegate, basic UI for login.

---
+++ OnTheMap/GCDBlackBox.swift
@@ -0,0 +1,16 @@
+//
+//  GCDBlackBox.swift
+//  OnTheMap
+//
+//  Created by Shane Sealy on 7/20/18.
+//  Copyright © 2018 Shane Sealy. All rights reserved.
+//
+
+import Foundation
+
+func performUIUpdatesOnMain(_ updates: @escaping () -> Void) {
+    DispatchQueue.main.async {
+        updates()
+    }
+}
+
--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    // Mark: IBOutlets
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    // Mark: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
