91eab55374d2debaccc57282d040da142549a32b
Shane Sealy <35928028+GSD4life@users.noreply.github.com>
2018-08-04 10:19:30 -0400

    Added back two swift files

---
+++ OnTheMap/Model/Clients/Udacity/UdacityClient.swift
@@ -0,0 +1,9 @@
+//
+//  UdacityClient.swift
+//  OnTheMap
+//
+//  Created by Shane Sealy on 8/4/18.
+//  Copyright © 2018 Shane Sealy. All rights reserved.
+//
+
+import Foundation
---
+++ OnTheMap/Model/Clients/Udacity/UdacityConstants.swift
@@ -0,0 +1,21 @@
+//
+//  UdacityConstants.swift
+//  OnTheMap
+//
+//  Created by Shane Sealy on 8/4/18.
+//  Copyright © 2018 Shane Sealy. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    // Mark: Udacity
+    struct URL {
+        static let ApiScheme = "https"
+        static let ApiHost = "www.udacity.com"
+        static let ApiPath = "/api"
+}
+
+}
