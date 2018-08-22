31a333db94f9da06b2649ee3133777d2ae15c840
Shane Sealy <35928028+GSD4life@users.noreply.github.com>
2018-08-22 14:13:09 -0400

    Added logout function to TabBarController

--- OnTheMap/View Controllers/ManagerTabBarController.swift
+++ OnTheMap/View Controllers/ManagerTabBarController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ManagerTabBarController: UITabBarController {
+    
+var refresh = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setUpTabBar()
+        tabBarButtons()
         
     }
     
-    func setUpTabBar() {
+    func tabBarButtons() {
         navigationItem.title = "On The Map"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "logout", style: .plain, target: self, action: #selector(cancel))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "logout", style: .plain, target: self, action: #selector(logout))
+        
         
         let refreshButton = UIButton(type: .system)
         refreshButton.setImage(#imageLiteral(resourceName: "icon_refresh"), for: .normal)
@@ -34,9 +37,32 @@ class ManagerTabBarController: UITabBarController {
         
     }
     
-    @objc func cancel() {
+    @objc func cancel(sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    
+    
+    @objc func logout() {
+     UdacityClient.sharedInstance().logout(completionHandlerToLogout: { (data, error) in
+        
+        if error != nil {
+            
+            let alert = UIAlertController(title: "Logout Failed", message: "Please try again.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+            
+            self.present(alert, animated: true)
+            
+        } else {
+            
+            self.cancel(sender: data!)
+        }
+        
+        
+        }
+    )}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
